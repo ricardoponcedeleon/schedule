878fe738cbc3c2b107b5faab7cad88fd851066ed
ricardoponcedeleon <poncedeleon.richie@gmail.com>
2018-12-03 16:06:31 -0700

    app setup

---
+++ src/bootstrap.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/home';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
